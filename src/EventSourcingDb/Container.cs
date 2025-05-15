5faac96e175642a99176626bbd9f68eba297d079
Golo Roden <golo.roden@thenativeweb.io>
2025-05-15 23:14:10 +0200

    Implement first tests.

---
+++ src/EventSourcingDb/Container.cs
@@ -0,0 +1,98 @@
+namespace EventSourcingDb;
+
+using System;
+using System.Threading.Tasks;
+using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Containers;
+using DotNet.Testcontainers.Configurations;
+
+public class Container
+{
+    private string _imageName = "thenativeweb/eventsourcingdb";
+    private string _imageTag = "latest";
+    private int _internalPort = 3000;
+    private string _apiToken = "secret";
+    private IContainer? _container;
+
+    public Container WithImageTag(string tag)
+    {
+        this._imageTag = tag;
+        return this;
+    }
+
+    public Container WithApiToken(string token)
+    {
+        this._apiToken = token;
+        return this;
+    }
+
+    public Container WithPort(int port)
+    {
+        this._internalPort = port;
+        return this;
+    }
+
+    public async Task StartAsync()
+    {
+        var builder = new ContainerBuilder()
+            .WithImage($"{this._imageName}:{this._imageTag}")
+            .WithExposedPort(this._internalPort)
+            .WithPortBinding(this._internalPort, assignRandomHostPort: true)
+            .WithCommand(
+                "run",
+                "--api-token", this._apiToken,
+                "--data-directory-temporary",
+                "--http-enabled",
+                "--https-enabled=false"
+            )
+            .WithWaitStrategy(Wait.ForUnixContainer()
+                .UntilHttpRequestIsSucceeded(request =>
+                    request.ForPort(this._internalPort)
+                           .ForPath("/api/v1/ping")))
+            .WithCleanUp(true);
+
+        this._container = builder.Build();
+        await this._container.StartAsync();
+    }
+
+    public string GetHost()
+    {
+        return this._container?.Hostname ?? throw new InvalidOperationException("Container must be running.");
+    }
+
+    public int GetMappedPort()
+    {
+        return this._container?.GetMappedPublicPort(this._internalPort)
+               ?? throw new InvalidOperationException("Container must be running.");
+    }
+
+    public Uri GetBaseUrl()
+    {
+        return new Uri($"http://{this.GetHost()}:{this.GetMappedPort()}");
+    }
+
+    public string GetApiToken()
+    {
+        return this._apiToken;
+    }
+
+    public bool IsRunning()
+    {
+        return this._container?.State == TestcontainersStates.Running;
+    }
+
+    public async Task StopAsync()
+    {
+        if (this._container is not null)
+        {
+            await this._container.StopAsync();
+            await this._container.DisposeAsync();
+            this._container = null;
+        }
+    }
+
+    public Client GetClient()
+    {
+        return new Client(this.GetBaseUrl(), this.GetApiToken());
+    }
+}
