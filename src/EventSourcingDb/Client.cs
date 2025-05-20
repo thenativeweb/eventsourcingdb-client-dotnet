cf231a13e5f1d7796a2d48db2c154e3d0e7b688c
Golo Roden <golo.roden@thenativeweb.io>
2025-05-20 10:39:24 +0200

    Use correct class.

--- src/EventSourcingDb/Client.cs
+++ src/EventSourcingDb/Client.cs
@@ -9,7 +9,7 @@ public class Client
 {
     private readonly Uri _baseUrl;
     private readonly string _apiToken;
-    private static readonly HttpClient _httpClient = new
+    private static readonly HttpClient _httpClient = new HttpClient
     {
         PooledConnectionLifetime = TimeSpan.FromMinutes(2),
     };
