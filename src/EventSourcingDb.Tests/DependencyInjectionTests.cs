68a876c9acfe4baef37bf6bdb3f078c59192ef90
Chris Dörnen <mail@christiandoernen.de>
2025-09-09 23:32:11 +0200

    Add support for microsoft dependency injection

---
+++ src/EventSourcingDb.Tests/DependencyInjectionTests.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using EventSourcingDb.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace EventSourcingDb.Tests;
+
+public sealed class DependencyInjectionTests : IAsyncLifetime
+{
+    private Container? _container;
+
+    public async Task InitializeAsync()
+    {
+        var imageVersion = DockerfileHelper.GetImageVersionFromDockerfile();
+        _container = new Container().WithImageTag(imageVersion);
+        await _container.StartAsync();
+    }
+
+    public async Task DisposeAsync()
+    {
+        if (_container is not null)
+        {
+            await _container.StopAsync();
+        }
+    }
+
+    [Fact]
+    public async Task ClientIsAvailableUsingDependencyInjection()
+    {
+        var url = _container!.GetBaseUrl();
+        var apiToken = _container!.GetApiToken();
+
+        var configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(
+                [
+                    new KeyValuePair<string, string?>("EventSourcingDb:BaseUrl", url.ToString()),
+                    new KeyValuePair<string, string?>("EventSourcingDb:ApiToken", apiToken)
+                ]
+            )
+            .Build();
+
+        var services = new ServiceCollection();
+        services.AddLogging();
+        services.AddEventSourcingDb(configuration);
+
+        var serviceProvider = services.BuildServiceProvider();
+
+        var client = serviceProvider.GetService<Client>();
+
+        await client!.PingAsync();
+    }
+
+}
---
+++ src/EventSourcingDb/DependencyInjection/EventSourcingDbOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EventSourcingDb.DependencyInjection;
+
+public class EventSourcingDbOptions
+{
+    [Required]
+    public required string ApiToken { get; set; }
+
+    [Required, Url]
+    public required Uri BaseUrl { get; set; }
+}
