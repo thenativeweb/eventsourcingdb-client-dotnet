f7cb8aebafd7c3ac2fe6aba735c835f6713bf3e5
Chris Dörnen <mail@christiandoernen.de>
2025-10-04 13:07:29 +0200

    Refactoring: extract common checks to internal extension methods, authorize http client once, use literal equality checks where possible, rename "line" variables

---
+++ src/EventSourcingDb/HttpResponseExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EventSourcingDb;
+
+public static class HttpResponseExtensions
+{
+    public static async Task ThrowIfNotSuccessStatusCode(
+        this HttpResponseMessage response,
+        CancellationToken cancellationToken = default
+    )
+    {
+        if (response.StatusCode is not HttpStatusCode.OK)
+        {
+            var errorResponse = await response.Content.ReadAsStringAsync(cancellationToken).ConfigureAwait(false);
+            throw new HttpRequestException(
+                message: $"Unexpected status code ('{errorResponse}').", inner: null, statusCode: response.StatusCode
+            );
+        }
+    }
+}
