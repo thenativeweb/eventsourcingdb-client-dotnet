19827ae7dfc6bd7f77083a600705b4b3e508ba52
Golo Roden <golo.roden@thenativeweb.io>
2025-10-20 23:52:21 +0200

    feat: Add isSubjectPopulated precondition

--- src/EventSourcingDb/Types/Precondition.cs
+++ src/EventSourcingDb/Types/Precondition.cs
@@ -11,6 +11,9 @@ public record Precondition
     public static Precondition IsSubjectPristinePrecondition(string subject) =>
         new Precondition { Type = "isSubjectPristine", Payload = new IsSubjectPristinePrecondition(subject) };
 
+    public static Precondition IsSubjectPopulatedPrecondition(string subject) =>
+        new Precondition { Type = "isSubjectPopulated", Payload = new IsSubjectPopulatedPrecondition(subject) };
+
     public static Precondition IsSubjectOnEventIdPrecondition(string subject, string eventId) =>
         new Precondition { Type = "isSubjectOnEventId", Payload = new IsSubjectOnEventIdPrecondition(subject, eventId) };
 
